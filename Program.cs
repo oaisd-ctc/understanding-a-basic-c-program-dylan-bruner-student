3e18cbdde43ba3fec9df541d4ab081f17b0429e8
dylan-bruner-student <dybr78f@student.oaisd.org>
2024-09-06 17:59:38 +0000

    done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello world!");
+        Console.WriteLine("My name is Dylan Bruner.");
+
+        int age = 17;
+        string greeting = "Hello, world!";
+        string firstname = "Dylan";
+        string lastname = "Bruner";
+        double money = 2.79;
+
+        Console.WriteLine($"{greeting}! My name is {firstname} {lastname}. I am {age} years old. I just spent ${money} on an Energy Drink.");
+    }
+}
